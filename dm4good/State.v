e0f9bc52f4a15e99f196f026aaaa94fd91c237aa
Théo Winterhalter <theo.winterhalter@gmail.com>
2022-02-15 17:35:20 +0100

    Define also partial state following new idea

---
+++ dm4good/State.v
@@ -0,0 +1,282 @@
+(* Define a partial Dijkstra monad for state *)
+
+From Coq Require Import Utf8 RelationClasses.
+
+Set Default Goal Selector "!".
+Set Printing Projections.
+
+Ltac forward_gen H tac :=
+  match type of H with
+  | ?X → _ => let H' := fresh in assert (H':X) ; [tac|specialize (H H'); clear H']
+  end.
+
+Tactic Notation "forward" constr(H) := forward_gen H ltac:(idtac).
+Tactic Notation "forward" constr(H) "by" tactic(tac) := forward_gen H tac.
+
+Notation val x := (let 'exist _ t _ := x in t).
+Notation "⟨ u ⟩" := (exist _ u _).
+
+Definition prf [A P] (p : { x : A | P x }) : P (val p).
+Proof.
+  destruct p. assumption.
+Qed.
+
+Notation "'∑' x .. y , p" := (sigT (fun x => .. (sigT (fun y => p%type)) ..))
+  (at level 200, x binder, right associativity,
+   format "'[' '∑'  '/  ' x  ..  y ,  '/  ' p ']'")
+  : type_scope.
+
+Notation "( x ; y )" := (@existT _ _ x y).
+Notation "( x ; y ; z )" := (x ; ( y ; z)).
+Notation "( x ; y ; z ; t )" := (x ; ( y ; (z ; t))).
+Notation "( x ; y ; z ; t ; u )" := (x ; ( y ; (z ; (t ; u)))).
+Notation "( x ; y ; z ; t ; u ; v )" := (x ; ( y ; (z ; (t ; (u ; v))))).
+Notation "x .π1" := (@projT1 _ _ x) (at level 3, format "x '.π1'").
+Notation "x .π2" := (@projT2 _ _ x) (at level 3, format "x '.π2'").
+
+Section State.
+
+  Context (state : Type).
+
+  (* Guarded monad *)
+
+  Definition G A :=
+    ∑ (P : Prop), P → A.
+
+  Definition retᴳ [A] (x : A) : G A :=
+    (True ; λ _, x).
+
+  Definition bindᴳ [A B] (x : G A) (f : A → G B) : G B.
+  Proof.
+    exists (∃ (h : x.π1), (f (x.π2 h)).π1).
+    simple refine (λ h, (f (x.π2 _)).π2 _).
+    - destruct h. assumption.
+    - destruct h as [h hf]. assumption.
+  Defined.
+
+  Definition reqᴳ [A] (p : Prop) (x : p → A) : G A :=
+    (p ; x).
+
+  (* Computation monad *)
+
+  Definition M A := state → G (state * A).
+
+  Definition retᴹ [A] (x : A) : M A :=
+    λ s₀, retᴳ (s₀, x).
+
+  Definition bindᴹ [A B] (c : M A) (f : A → M B) : M B :=
+    λ s₀, bindᴳ (c s₀) (λ '(s₁, x), f x s₁).
+
+  Definition getᴹ : M state :=
+    λ s, retᴳ (s, s).
+
+  Definition putᴹ (s : state) : M unit :=
+    λ s₀, retᴳ (s, tt).
+
+  Definition reqᴹ (p : Prop) : M p :=
+    λ s₀, reqᴳ p (λ h, (s₀, h)).
+
+  (* Specification monad *)
+
+  Definition preᵂ := state → Prop.
+  Definition postᵂ A := state → A → Prop.
+
+  Definition W A := postᵂ A → preᵂ.
+
+  Definition wle [A] (w₀ w₁ : W A) : Prop :=
+    ∀ P s, w₁ P s → w₀ P s.
+
+  Notation "x ≤ᵂ y" := (wle x y) (at level 80).
+
+  Definition retᵂ [A] (x : A) : W A :=
+    λ P s₀, P s₀ x.
+
+  Definition bindᵂ [A B] (w : W A) (wf : A → W B) : W B :=
+    λ P, w (λ s₁ x, wf x P s₁).
+
+  Definition getᵂ : W state :=
+    λ P s, P s s.
+
+  Definition putᵂ (s : state) : W unit :=
+    λ P s₀, P s tt.
+
+  Definition reqᵂ (p : Prop) : W p :=
+    λ P s₀, ∃ (h : p), P s₀ h.
+
+  Instance trans [A] : Transitive (@wle A).
+  Proof.
+    intros x y z h₁ h₂. intros P s₀ h.
+    apply h₁. apply h₂.
+    assumption.
+  Qed.
+
+  (* Monotonicity *)
+
+  Class Monotonous [A] (w : W A) :=
+    ismono : ∀ (P Q : postᵂ A) s₀, (∀ s₁ x, P s₁ x → Q s₁ x) → w P s₀ → w Q s₀.
+
+  Instance retᵂ_ismono [A] (x : A) : Monotonous (retᵂ x).
+  Proof.
+    intros P Q s₀ hPQ h.
+    apply hPQ. apply h.
+  Qed.
+
+  Instance bindᵂ_ismono [A B] (w : W A) (wf : A → W B) :
+    Monotonous w →
+    (∀ x, Monotonous (wf x)) →
+    Monotonous (bindᵂ w wf).
+  Proof.
+    intros mw mwf.
+    intros P Q s₀ hPQ h.
+    eapply mw. 2: exact h.
+    simpl. intros s₁ x hf.
+    eapply mwf. 2: exact hf.
+    assumption.
+  Qed.
+
+  Instance getᵂ_ismono : Monotonous (getᵂ).
+  Proof.
+    intros P Q s₀ hPQ h.
+    red. red in h.
+    apply hPQ. assumption.
+  Qed.
+
+  Instance putᵂ_ismono : ∀ s, Monotonous (putᵂ s).
+  Proof.
+    intros s. intros P Q s₀ hPQ h.
+    apply hPQ. assumption.
+  Qed.
+
+  Instance reqᵂ_ismono : ∀ p, Monotonous (reqᵂ p).
+  Proof.
+    intros p. intros P Q s₀ hPQ h.
+    destruct h as [hp h].
+    exists hp. apply hPQ. assumption.
+  Qed.
+
+  Lemma bindᵂ_mono :
+    ∀ [A B] (w w' : W A) (wf wf' : A → W B),
+      Monotonous w' →
+      w ≤ᵂ w' →
+      (∀ x, wf x ≤ᵂ wf' x) →
+      bindᵂ w wf ≤ᵂ bindᵂ w' wf'.
+  Proof.
+    intros A B w w' wf wf' mw' hw hwf.
+    intros P s₀ h.
+    red. red in h.
+    apply hw. eapply mw'. 2: exact h.
+    simpl. intros s₁ x hf. apply hwf. assumption.
+  Qed.
+
+  (* Effect observation *)
+
+  Definition θ [A] (c : M A) : W A :=
+    λ post s₀,
+      let '(p ; f) := c s₀ in
+      ∃ (h : p), let '(s₁, x) := f h in post s₁ x.
+
+  Lemma θ_ret :
+    ∀ A (x : A),
+      θ (retᴹ x) ≤ᵂ retᵂ x.
+  Proof.
+    intros A x. intros post s₀ h.
+    cbn. exists I. red in h. assumption.
+  Qed.
+
+  Lemma θ_bind :
+    ∀ A B c f,
+      θ (@bindᴹ A B c f) ≤ᵂ bindᵂ (θ c) (λ x, θ (f x)).
+  Proof.
+    intros A B c f. intros post s₀ h.
+    red. red. red in h. red in h.
+    destruct (c s₀) as [p c']. clear c.
+    destruct h as [hp h].
+    simpl.
+    unshelve eexists.
+    { exists hp. destruct (c' hp) as [s₁ x]. red in h. destruct (f x s₁).
+      simpl. destruct h. assumption.
+    }
+    simpl. destruct (c' hp) as [s₁ x]. red in h. destruct (f x s₁).
+    simpl. destruct h. assumption.
+  Qed.
+
+  (* Partial Dijkstra monad *)
+
+  Definition D A w :=
+    { c : M A | θ c ≤ᵂ w }.
+
+  Definition retᴰ [A] (x : A) : D A (retᵂ x).
+  Proof.
+    exists (retᴹ x).
+    apply θ_ret.
+  Defined.
+
+  Definition bindᴰ [A B w wf] (c : D A w) (f : ∀ x, D B (wf x))
+    `{Monotonous _ w} :
+    D B (bindᵂ w wf).
+  Proof.
+    exists (bindᴹ (val c) (λ x, val (f x))).
+    etransitivity. 1: apply θ_bind.
+    apply bindᵂ_mono.
+    - assumption.
+    - destruct c. assumption.
+    - intro x. destruct (f x). assumption.
+  Qed.
+
+  Definition subcompᴰ [A w w'] (c : D A w) {h : w ≤ᵂ w'} : D A w'.
+  Proof.
+    exists (val c).
+    etransitivity. 2: exact h.
+    destruct c. assumption.
+  Defined.
+
+  Definition getᴰ : D state getᵂ.
+  Proof.
+    exists getᴹ.
+    cbv. intros post s h. exists I. assumption.
+  Defined.
+
+  Definition putᴰ s : D unit (putᵂ s).
+  Proof.
+    exists (putᴹ s).
+    cbv. intros post s₀ h. exists I. assumption.
+  Defined.
+
+  Definition reqᴰ (p : Prop) : D p (reqᵂ p).
+  Proof.
+    exists (reqᴹ p).
+    cbv. auto.
+  Defined.
+
+  (* Lift from PURE (somehow) *)
+
+  Definition pure_wp' A := (A → Prop) → Prop.
+
+  Definition pure_mono [A] (w : pure_wp' A) : Prop :=
+    ∀ (P Q : A → Prop), (∀ x, P x → Q x) → w P → w Q.
+
+  Definition pure_wp A :=
+    { w : pure_wp' A | pure_mono w }.
+
+  Definition PURE A (w : pure_wp A) :=
+    val w (λ _, True) → { x : A | ∀ P, val w P → P x }.
+
+  Definition liftᵂ [A] (w : pure_wp A) : W A :=
+    λ P s₀, val w (λ x, P s₀ x).
+
+  Definition liftᴾ [A w] (f : PURE A w) : D A (liftᵂ w).
+  Proof.
+    refine (subcompᴰ (bindᴰ (reqᴰ (val w (λ _, True))) (λ h, retᴰ (val (f h))))).
+    intros P s h.
+    assert (hpre : val w (λ _, True)).
+    { unfold liftᵂ in h.
+      destruct w as [w hw].
+      eapply hw. 2: exact h.
+      auto.
+    }
+    cbv. exists hpre.
+    pose proof (prf (f hpre)) as hf. simpl in hf.
+    apply hf in h. assumption.
+  Defined.
+
+End State.
---
+++ dm4good/StateFree.v
@@ -0,0 +1,279 @@
+(* Define a partial Dijkstra monad from a free monad for state *)
+
+From Coq Require Import Utf8 RelationClasses.
+
+Set Default Goal Selector "!".
+Set Printing Projections.
+
+Ltac forward_gen H tac :=
+  match type of H with
+  | ?X → _ => let H' := fresh in assert (H':X) ; [tac|specialize (H H'); clear H']
+  end.
+
+Tactic Notation "forward" constr(H) := forward_gen H ltac:(idtac).
+Tactic Notation "forward" constr(H) "by" tactic(tac) := forward_gen H tac.
+
+Notation val x := (let 'exist _ t _ := x in t).
+Notation "⟨ u ⟩" := (exist _ u _).
+
+Definition prf [A P] (p : { x : A | P x }) : P (val p).
+Proof.
+  destruct p. assumption.
+Qed.
+
+Notation "'∑' x .. y , p" := (sigT (fun x => .. (sigT (fun y => p%type)) ..))
+  (at level 200, x binder, right associativity,
+   format "'[' '∑'  '/  ' x  ..  y ,  '/  ' p ']'")
+  : type_scope.
+
+Notation "( x ; y )" := (@existT _ _ x y).
+Notation "( x ; y ; z )" := (x ; ( y ; z)).
+Notation "( x ; y ; z ; t )" := (x ; ( y ; (z ; t))).
+Notation "( x ; y ; z ; t ; u )" := (x ; ( y ; (z ; (t ; u)))).
+Notation "( x ; y ; z ; t ; u ; v )" := (x ; ( y ; (z ; (t ; (u ; v))))).
+Notation "x .π1" := (@projT1 _ _ x) (at level 3, format "x '.π1'").
+Notation "x .π2" := (@projT2 _ _ x) (at level 3, format "x '.π2'").
+
+Section State.
+
+  Context (state : Type).
+
+  (* Computation monad with an extra require constructor *)
+
+  Inductive M A :=
+  | retᴹ (x : A)
+  | act_getᴹ (k : state → M A)
+  | act_putᴹ (s : state) (k : M A)
+  | act_reqᴹ (p : Prop) (k : p → M A).
+
+  Arguments retᴹ [_].
+  Arguments act_getᴹ [_].
+  Arguments act_putᴹ [_].
+  Arguments act_reqᴹ [_].
+
+  Fixpoint bindᴹ [A B] (c : M A) (f : A → M B) : M B :=
+    match c with
+    | retᴹ x => f x
+    | act_getᴹ k => act_getᴹ (λ s, bindᴹ (k s) f)
+    | act_putᴹ s k => act_putᴹ s (bindᴹ k f)
+    | act_reqᴹ p k => act_reqᴹ p (λ h, bindᴹ (k h) f)
+    end.
+
+  Definition getᴹ : M state :=
+    act_getᴹ (λ x, retᴹ x).
+
+  Definition putᴹ (s : state) : M unit :=
+    act_putᴹ s (retᴹ tt).
+
+  Definition reqᴹ (p : Prop) : M p :=
+    act_reqᴹ p (λ h, retᴹ h).
+
+  (* Specification monad *)
+
+  Definition preᵂ := state → Prop.
+  Definition postᵂ A := state → A → Prop.
+
+  Definition W A := postᵂ A → preᵂ.
+
+  Definition wle [A] (w₀ w₁ : W A) : Prop :=
+    ∀ P s, w₁ P s → w₀ P s.
+
+  Notation "x ≤ᵂ y" := (wle x y) (at level 80).
+
+  Definition retᵂ [A] (x : A) : W A :=
+    λ P s₀, P s₀ x.
+
+  Definition bindᵂ [A B] (w : W A) (wf : A → W B) : W B :=
+    λ P, w (λ s₁ x, wf x P s₁).
+
+  Definition getᵂ : W state :=
+    λ P s, P s s.
+
+  Definition putᵂ (s : state) : W unit :=
+    λ P s₀, P s tt.
+
+  Definition reqᵂ (p : Prop) : W p :=
+    λ P s₀, ∃ (h : p), P s₀ h.
+
+  Instance trans [A] : Transitive (@wle A).
+  Proof.
+    intros x y z h₁ h₂. intros P s₀ h.
+    apply h₁. apply h₂.
+    assumption.
+  Qed.
+
+  (* Monotonicity *)
+
+  Class Monotonous [A] (w : W A) :=
+    ismono : ∀ (P Q : postᵂ A) s₀, (∀ s₁ x, P s₁ x → Q s₁ x) → w P s₀ → w Q s₀.
+
+  Instance retᵂ_ismono [A] (x : A) : Monotonous (retᵂ x).
+  Proof.
+    intros P Q s₀ hPQ h.
+    apply hPQ. apply h.
+  Qed.
+
+  Instance bindᵂ_ismono [A B] (w : W A) (wf : A → W B) :
+    Monotonous w →
+    (∀ x, Monotonous (wf x)) →
+    Monotonous (bindᵂ w wf).
+  Proof.
+    intros mw mwf.
+    intros P Q s₀ hPQ h.
+    eapply mw. 2: exact h.
+    simpl. intros s₁ x hf.
+    eapply mwf. 2: exact hf.
+    assumption.
+  Qed.
+
+  Instance getᵂ_ismono : Monotonous (getᵂ).
+  Proof.
+    intros P Q s₀ hPQ h.
+    red. red in h.
+    apply hPQ. assumption.
+  Qed.
+
+  Instance putᵂ_ismono : ∀ s, Monotonous (putᵂ s).
+  Proof.
+    intros s. intros P Q s₀ hPQ h.
+    apply hPQ. assumption.
+  Qed.
+
+  Instance reqᵂ_ismono : ∀ p, Monotonous (reqᵂ p).
+  Proof.
+    intros p. intros P Q s₀ hPQ h.
+    destruct h as [hp h].
+    exists hp. apply hPQ. assumption.
+  Qed.
+
+  Lemma bindᵂ_mono :
+    ∀ [A B] (w w' : W A) (wf wf' : A → W B),
+      Monotonous w' →
+      w ≤ᵂ w' →
+      (∀ x, wf x ≤ᵂ wf' x) →
+      bindᵂ w wf ≤ᵂ bindᵂ w' wf'.
+  Proof.
+    intros A B w w' wf wf' mw' hw hwf.
+    intros P s₀ h.
+    red. red in h.
+    apply hw. eapply mw'. 2: exact h.
+    simpl. intros s₁ x hf. apply hwf. assumption.
+  Qed.
+
+  (* Effect observation
+    Here I don't do it in two passes because of the req.
+  *)
+
+  Fixpoint θ [A] (c : M A) : W A :=
+    match c with
+    | retᴹ x => retᵂ x
+    | act_getᴹ k => bindᵂ getᵂ (λ x, θ (k x))
+    | act_putᴹ s k => bindᵂ (putᵂ s) (λ _, θ k)
+    | act_reqᴹ p k => bindᵂ (reqᵂ p) (λ x, θ (k x))
+    end.
+
+  Lemma θ_ret :
+    ∀ A (x : A),
+      θ (retᴹ x) ≤ᵂ retᵂ x.
+  Proof.
+    intros A x. intros P s₀ h.
+    cbn. red in h. assumption.
+  Qed.
+
+  Lemma θ_bind :
+    ∀ A B c f,
+      θ (@bindᴹ A B c f) ≤ᵂ bindᵂ (θ c) (λ x, θ (f x)).
+  Proof.
+    intros A B c f.
+    induction c as [| ? ih | ?? ih | ?? ih] in B, f |- *.
+    - cbn. intros P s₀ h.
+      assumption.
+    - cbn. intros P s₀ h.
+      apply ih. assumption.
+    - cbn. intros P s₀ h.
+      apply ih. assumption.
+    - cbn. intros P s₀ h.
+      red. red. destruct h as [hp h].
+      exists hp. apply ih. assumption.
+  Qed.
+
+  (* Partial Dijkstra monad *)
+
+  Definition D A w :=
+    { c : M A | θ c ≤ᵂ w }.
+
+  Definition retᴰ [A] (x : A) : D A (retᵂ x).
+  Proof.
+    exists (retᴹ x).
+    apply θ_ret.
+  Defined.
+
+  Definition bindᴰ [A B w wf] (c : D A w) (f : ∀ x, D B (wf x))
+    `{Monotonous _ w} :
+    D B (bindᵂ w wf).
+  Proof.
+    exists (bindᴹ (val c) (λ x, val (f x))).
+    etransitivity. 1: apply θ_bind.
+    apply bindᵂ_mono.
+    - assumption.
+    - destruct c. assumption.
+    - intro x. destruct (f x). assumption.
+  Qed.
+
+  Definition subcompᴰ [A w w'] (c : D A w) {h : w ≤ᵂ w'} : D A w'.
+  Proof.
+    exists (val c).
+    etransitivity. 2: exact h.
+    destruct c. assumption.
+  Defined.
+
+  Definition getᴰ : D state getᵂ.
+  Proof.
+    exists getᴹ.
+    cbv. auto.
+  Defined.
+
+  Definition putᴰ s : D unit (putᵂ s).
+  Proof.
+    exists (putᴹ s).
+    cbv. auto.
+  Defined.
+
+  Definition reqᴰ (p : Prop) : D p (reqᵂ p).
+  Proof.
+    exists (reqᴹ p).
+    cbv. auto.
+  Defined.
+
+  (* Lift from PURE (somehow) *)
+
+  Definition pure_wp' A := (A → Prop) → Prop.
+
+  Definition pure_mono [A] (w : pure_wp' A) : Prop :=
+    ∀ (P Q : A → Prop), (∀ x, P x → Q x) → w P → w Q.
+
+  Definition pure_wp A :=
+    { w : pure_wp' A | pure_mono w }.
+
+  Definition PURE A (w : pure_wp A) :=
+    val w (λ _, True) → { x : A | ∀ P, val w P → P x }.
+
+  Definition liftᵂ [A] (w : pure_wp A) : W A :=
+    λ P s₀, val w (λ x, P s₀ x).
+
+  Definition liftᴾ [A w] (f : PURE A w) : D A (liftᵂ w).
+  Proof.
+    refine (subcompᴰ (bindᴰ (reqᴰ (val w (λ _, True))) (λ h, retᴰ (val (f h))))).
+    intros P s h.
+    assert (hpre : val w (λ _, True)).
+    { unfold liftᵂ in h.
+      destruct w as [w hw].
+      eapply hw. 2: exact h.
+      auto.
+    }
+    cbv. exists hpre.
+    pose proof (prf (f hpre)) as hf. simpl in hf.
+    apply hf in h. assumption.
+  Defined.
+
+End State.
