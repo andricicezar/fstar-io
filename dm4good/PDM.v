7405e6bac53c80ea31b3e19df5f65337cb61e7f4
Théo Winterhalter <theo.winterhalter@gmail.com>
2022-02-16 12:20:57 +0100

    Package concepts in general construction

--- dm4good/PDM.v
+++ dm4good/PDM.v
@@ -7,86 +7,94 @@ Set Default Goal Selector "!".
 Set Printing Projections.
 Set Universe Polymorphism.
 
+(* Monad with a require operator (not checking laws) *)
+Record ReqMonad := {
+  M :> Type → Type ;
+  ret : ∀ A (x : A), M A ;
+  bind : ∀ A B (c : M A) (f : A → M B), M B ;
+  req : ∀ (p : Prop), M p
+}.
+
+Arguments ret _ [_].
+Arguments bind _ [_ _].
+
+(* Relevant so should be Order *)
+Class Ordered (W : ReqMonad) := {
+  wle : ∀ A, W A → W A → Prop ;
+  trans : ∀ A, Transitive (@wle A)
+}.
+
+Arguments wle {_ _ _}.
+
+Notation "x ≤ᵂ y" := (wle x y) (at level 80).
+
+Class MonoSpec (W : ReqMonad) (Word : Ordered W) := {
+  bind_mono :
+    ∀ A B (w w' : W A) (wf wf' : A → W B),
+      w ≤ᵂ w' →
+      (∀ x, wf x ≤ᵂ wf' x) →
+      W.(bind) w wf ≤ᵂ W.(bind) w' wf'
+}.
+
+Definition observation (M W : ReqMonad) :=
+  ∀ A (c : M A), W A.
+
+Class LaxMorphism {M W} (Word : Ordered W) (θ : observation M W) := {
+  θ_ret :
+    ∀ A (x : A),
+      θ _ (M.(ret) x) ≤ᵂ W.(ret) x ;
+  θ_bind :
+    ∀ A B c f,
+      θ _ (M.(@bind) A B c f) ≤ᵂ W.(bind) (θ _ c) (λ x, θ _ (f x)) ;
+  θ_req :
+    ∀ p, θ _ (M.(req) p) ≤ᵂ W.(req) p
+}.
+
+Definition spec_lift_pure (W : ReqMonad) :=
+  ∀ A (w : pure_wp A), W A.
+
+Class PureSpec (W : ReqMonad) (Word : Ordered W) (liftᵂ : spec_lift_pure W) := {
+  req_lift :
+    ∀ A w (f : PURE A w),
+      W.(bind) (W.(req) (val w (λ _, True))) (λ h, W.(ret) (val (f h))) ≤ᵂ
+      liftᵂ _ w
+}.
+
 Section PDM.
 
   (* Computational monad with assert/req *)
 
-  Context (M : Type → Type).
-  Context (retᴹ : ∀ A (x : A), M A).
-  Context (bindᴹ : ∀ A B (c : M A) (f : A → M B), M B).
-  Context (reqᴹ : ∀ (p : Prop), M p).
-
-  Arguments retᴹ [_].
-  Arguments bindᴹ [_ _].
+  Context (M : ReqMonad).
 
   (* Specification monad *)
 
-  Context (W : Type → Type).
-  Context (wle : ∀ [A], W A → W A → Prop).
+  Context (W : ReqMonad) (Word : Ordered W) (hmono : MonoSpec W Word).
 
-  Arguments wle [_].
-
-  Notation "x ≤ᵂ y" := (wle x y) (at level 80).
-
-  Context (trans : ∀ A, Transitive (@wle A)).
-
-  Context (retᵂ : ∀ A (x : A), W A).
-  Context (bindᵂ : ∀ A B (w : W A) (wf : A → W B), W B).
-  Context (reqᵂ : ∀ (p : Prop), W p).
-
-  Arguments retᵂ [_].
-  Arguments bindᵂ [_ _].
-
-  (* Monotonicity *)
-
-  Context (
-    bindᵂ_mono :
-      ∀ A B (w w' : W A) (wf wf' : A → W B),
-        w ≤ᵂ w' →
-        (∀ x, wf x ≤ᵂ wf' x) →
-        bindᵂ w wf ≤ᵂ bindᵂ w' wf'
-  ).
+  Existing Instance trans.
 
   (* Effect observation *)
 
-  Context (θ : ∀ A (c : M A), W A).
+  Context (θ : observation M W) (hlax : LaxMorphism Word θ).
 
   Arguments θ [_].
-
-  Context (
-    θ_ret :
-      ∀ A (x : A),
-        θ (retᴹ x) ≤ᵂ retᵂ x
-  ).
-
-  Context (
-    θ_bind :
-      ∀ A B c f,
-        θ (@bindᴹ A B c f) ≤ᵂ bindᵂ (θ c) (λ x, θ (f x))
-  ).
-
-  Context (
-    θ_req :
-      ∀ p, θ (reqᴹ p) ≤ᵂ reqᵂ p
-  ).
 
   (* Partial Dijkstra monad *)
 
   Definition D A w :=
     { c : M A | θ c ≤ᵂ w }.
 
-  Definition retᴰ [A] (x : A) : D A (retᵂ x).
+  Definition retᴰ [A] (x : A) : D A (W.(ret) x).
   Proof.
-    exists (retᴹ x).
+    exists (M.(ret) x).
     apply θ_ret.
   Defined.
 
   Definition bindᴰ [A B w wf] (c : D A w) (f : ∀ x, D B (wf x)) :
-    D B (bindᵂ w wf).
+    D B (W.(bind) w wf).
   Proof.
-    exists (bindᴹ (val c) (λ x, val (f x))).
+    exists (M.(bind) (val c) (λ x, val (f x))).
     etransitivity. 1: apply θ_bind.
-    apply bindᵂ_mono.
+    apply bind_mono.
     - destruct c. assumption.
     - intro x. destruct (f x). assumption.
   Qed.
@@ -98,23 +106,17 @@ Section PDM.
     destruct c. assumption.
   Defined.
 
-  Definition reqᴰ (p : Prop) : D p (reqᵂ p).
+  Definition reqᴰ (p : Prop) : D p (W.(req) p).
   Proof.
-    exists (reqᴹ p).
+    exists (M.(req) p).
     apply θ_req.
   Defined.
 
   (* Lift from PURE *)
 
-  Context (liftᵂ : ∀ A (w : pure_wp A), W A).
+  Context (liftᵂ : spec_lift_pure W) (hlift : PureSpec W Word liftᵂ).
 
   Arguments liftᵂ [_].
-
-  Context (
-    req_lift :
-      ∀ A w (f : PURE A w),
-        bindᵂ (reqᵂ (val w (λ _, True))) (λ h, retᵂ (val (f h))) ≤ᵂ liftᵂ w
-  ).
 
   Definition liftᴾ [A w] (f : PURE A w) : D A (liftᵂ w).
   Proof.
