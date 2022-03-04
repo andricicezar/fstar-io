93593046f20b99a938a78258d60ebbea83997c12
Théo Winterhalter <theo.winterhalter@gmail.com>
2022-03-04 15:12:03 +0100

    Progress on DM4Free construction

--- dm4good/DM4Free.v
+++ dm4good/DM4Free.v
@@ -18,10 +18,34 @@ Section DM4Free.
   Definition M A :=
     T G A.
 
+  Instance Monad_M : Monad M.
+  Proof.
+    unfold M. apply transf_monad. exact _.
+  Defined.
+
+  (* Instead I could show that a monad transformer preserves ReqMonad *)
+  Instance ReqMonad_M : ReqMonad M := {|
+    req p := liftᵀ (req p)
+  |}.
+
   (* Specification monad *)
 
   Definition W A :=
     T pure_wp A.
+
+  Instance Monad_W : Monad W.
+  Proof.
+    apply transf_monad. exact _.
+  Defined.
+
+  Instance ReqMonad_W : ReqMonad W := {|
+    req p := liftᵀ (req p)
+  |}.
+
+  (* Not just a transform, but order-enriched transformer *)
+  Context (Order_W : Order W) (MonoSpec_W : MonoSpec W).
+
+  Context {hr : ∀ A, Reflexive (wle (A := A))}.
 
   (* Effect observation *)
 
@@ -45,5 +69,39 @@ Section DM4Free.
       + intros [h1 [h2 h3]].
         exists (ex_intro _ h1 h2). assumption.
   Defined.
+
+  Instance θ_lax : LaxMorphism θ.
+  Proof.
+    constructor.
+    - intros A x.
+      unfold θ. unshelve erewrite morph_ret. 1: exact _.
+      1:{ apply mapᵀ_morph. apply A. (* ?? *) }
+      reflexivity.
+    - intros A B c f.
+      unfold θ. unshelve erewrite morph_bind. 1: exact _.
+      1:{ apply mapᵀ_morph. apply A. (* ?? *) }
+      reflexivity.
+  Qed.
+
+  Instance θ_reqlax : ReqLaxMorphism Order_W θ.
+  Proof.
+    constructor.
+    intros p.
+    unfold θ.
+    (* Missing that it is a req monad morphism *)
+  Abort.
+
+  (* Partial Dijkstra monad *)
+
+  Definition D A w :=
+    PDM.D (θ := θ) (Word := Order_W) A w.
+
+  Instance DijkstraMonad_D : DijkstraMonad (Word := Order_W) D :=
+    PDM.DijkstraMonad_D MonoSpec_W θ_lax.
+
+  (* Lift from PURE *)
+
+  (* Definition liftᴾ :=
+    liftᴾ MonoSpec_W θ_lax θ_reqlax hlift. *)
 
 End DM4Free.
