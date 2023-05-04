adb5519cb2cbc0b9fee96065ef39f1b4ea33302d
Cezar Andrici <cezar.andrici@gmail.com>
2023-05-04 14:22:07 +0200

    Improve pretty printer

--- io/case-study/MIO_Sig_Call.ml
+++ io/case-study/MIO_Sig_Call.ml
@@ -7,44 +7,77 @@ let print_string2 str =
    print_string str; 
    flush stdout; true
 
+let print_caller (caller:bool) : unit =
+   if caller then print_string " Prog "
+   else print_string " Ctx "
+
 let print_event (e:event) : unit =
     match e with
        | EOpenfile (caller, arg, res) ->
-          print_string "EOpenfile;"
-       | ERead (caller, arg, res) ->
-          print_string "ERead;"
+          print_string "EOpenfile"; print_caller caller;
+          print_string "_ _;"
+       | ERead (caller, arg, res) -> begin
+          print_string "ERead"; print_caller caller;
+          let (fd,size) : UnixTypes.file_descr * int = Obj.magic arg in 
+          print_string "(";
+          print_int (Obj.magic fd);
+          print_string "\", ";
+          print_int (Obj.magic size);
+          print_string ") ";
+          let r : (bytes * int) resexn = Obj.magic res in 
+          match r with
+          | Inl (msg, size) -> begin
+            print_string "(Inl (\"";
+            print_string (Str.global_replace (Str.regexp "[\n\r]") "\\n" (Bytes.to_string msg));
+            print_string "\",";
+            print_int (Obj.magic size);
+            print_string "));"
+          end
+          | Inr _ -> print_string "(Inr _);"
+        end
        | EWrite (caller, arg, res) ->
           let (fd,msg) : UnixTypes.file_descr * bytes = Obj.magic arg in
-          print_string "EWrite ("; 
+          print_string "EWrite"; print_caller caller;
+          print_string "(";
           print_int (Obj.magic fd);
           print_string ",\"";
           print_string (Str.global_replace (Str.regexp "[\n\r]") "\\n" (Bytes.to_string msg));
           print_string "\") _;";
        | EClose (caller, arg, res) ->
-          print_string "EClose;"
+          print_string "EClose"; print_caller caller;
+          print_int (Obj.magic arg);
+          print_string " _;";
        | ESocket (caller, arg, res) ->
-          print_string "ESocket;"
+          print_string "ESocket _ _;"; print_caller caller;
+          print_string "_ _;"
        | ESetsockopt (caller, arg, res) ->
-          print_string "ESetsockopt;"
+          print_string "ESetsockopt"; print_caller caller;
+          print_string "_ _;"
        | EBind (caller, arg, res) ->
-          print_string "EBind;"
+          print_string "EBind"; print_caller caller;
+          print_string "_ _;"
        | ESetNonblock (caller, arg, res) ->
-          print_string "ESetNonBlock;"
+          print_string "ESetNonBlock"; print_caller caller;
+          print_string "_ _;"
        | EListen (caller, arg, res) ->
-          print_string "EListen;"
+          print_string "EListen"; print_caller caller;
+          print_string "_ _;"
        | EAccept (caller, arg, res) -> begin
+          print_string "EAccept"; print_caller caller;
           let r : UnixTypes.file_descr resexn = Obj.magic res in 
           match r with
           | Inl fd -> 
-            print_string "EAccept _ (Inl "; print_int (Obj.magic fd); print_string ");";
-          | Inr err -> print_string "EAccept _ (Inr _);"
+            print_string "_ (Inl "; print_int (Obj.magic fd); print_string ");";
+          | Inr err -> print_string "_ (Inr _);"
         end
        | ESelect (caller, arg, res) ->
           print_string ""
        | EAccess (caller, arg, res) ->
-          print_string "EAccess;"
+          print_string "EAccess"; print_caller caller;
+          print_string "_ _;"
        | EStat (caller, arg, res) ->
-          print_string "EStat;"
+          print_string "EStat"; print_caller caller;
+          print_string "_ _;"
 
 let rec print_trace0 (t:trace) : unit =
   match t with
