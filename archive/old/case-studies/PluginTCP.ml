76e49b190b9a73a5b0aa97822eff2d63efbbfce2
Cezar Andrici <cezar.andrici@gmail.com>
2023-09-26 12:00:01 +0200

    RM archive

--- archive/old/case-studies/PluginTCP.ml
+++
@@ -1,7 +0,0 @@
-let plugin (client:Types.file_descr) : unit =
-  let (msg, size) = IUnix.read client 250 in
-    if size < 1 then () 
-    else begin
-      let _ = IUnix.write client msg in
-      ()
-    end
