c0788f998d26a5cda8ee5705498df32c3953e1ee
Guido Martínez <mtzguido@gmail.com>
2025-02-28 02:09:08 -0800

    Bring in F* patches

---
+++ state/fstar-patches/anon.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Anonymize the patches
+
+sed -i 's/^From: .*/From: anon <anon>/' *.patch
+sed -i '/^From [0-9a-f]\{40\}/d' *.patch
+sed -i '/^index [0-9a-f]*\.\.[0-9a-f]* /d' *.patch
