6e775f368aa97e331f29211a08849c6de71eff53
Cezar Andrici <cezar.andrici@gmail.com>
2026-03-18 17:10:22 +0100

    copilot works based on token

--- seiostar/sandbox.entrypoint.sh
+++ seiostar/sandbox.entrypoint.sh
@@ -7,5 +7,6 @@ git config --global user.email "cezar.andrici@gmail.com"
 git config --global user.name "Cezar Andrici"
 
 echo "alias mycc='make verify -j 8 && claude --dangerously-skip-permissions'" > ~/.bashrc
+echo "alias myco='make verify -j 8 && copilot --yolo'" > ~/.bashrc
 
 exec "$@"
