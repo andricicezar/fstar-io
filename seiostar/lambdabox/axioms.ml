4cc5d39fab4a7819ecb3f2ed593ea64a4d6b12b2
E. Rivas <224161+erivas@users.noreply.github.com>
2026-03-28 23:43:10 -0300

    fixup! Trying to fix some nits in artifact

--- seiostar/lambdabox/axioms.ml
+++ seiostar/lambdabox/axioms.ml
@@ -59,10 +59,11 @@ let def_Runtime_io_read fd =
       let rec read_line () =
         let got = Unix.read ufd b 0 1 in
         if got = 0 then ()
-        else
+        else begin
           let c = Bytes.get b 0 in
-          if c = '\n' then ()
-          else begin Buffer.add_char buf c; read_line () end
+          Buffer.add_char buf c;
+          read_line ()
+        end
       in
       read_line ();
       make_inl (Obj.repr (Buffer.contents buf))
